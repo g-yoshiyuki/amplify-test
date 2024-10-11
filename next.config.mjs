5c28d40aac606403994a70c28e1e4e8327cb4ddf
g.yoshiyuki <yoshiyukigoubara@gmail.com>
2024-10-11 18:44:35 +0900

    微修正

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  env: {
+    MICROCMS_API_KEY: process.env.MICROCMS_API_KEY,
+    MICROCMS_SERVICE_DOMAIN: process.env.MICROCMS_SERVICE_DOMAIN,
+  },
+}
 
 export default nextConfig
